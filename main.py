02255c471d6fe02331d7a3275db03c2751177c29
frozenoffical <tenopno@gmail.com>
2025-05-05 13:05:17 +0530

    Update main.py

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ from pytgcalls.types.stream import StreamEnded
 app = Flask(__name__)
 
 # Download API URL
-DOWNLOAD_API_URL = "https://frozenyoutubeapisearchlink-production.up.railway.app/download?url="
+DOWNLOAD_API_URL = "https://frozen-youtube-api-search-link-b89x.onrender.com/download?url="
 
 # Caching setup for downloads
 download_cache = {}
