6e43f63fc23f51e2c60c3648039a5b61272867b4
frozenoffical <tenopno@gmail.com>
2025-06-27 15:22:24 +0530

    Update main.py

--- main.py
+++ main.py
@@ -16,8 +16,8 @@ app = Flask(__name__)
 
 # Define multiple Download APIs
 DOWNLOAD_APIS = {
-    'default': os.environ.get('DOWNLOAD_API_URL', 'http://68.183.106.44:5001/download?url='),
-    'secondary': os.environ.get('SECONDARY_DOWNLOAD_API_URL', 'http://68.183.106.44:5000/download?url='),
+    'default': os.environ.get('DOWNLOAD_API_URL', 'http://68.183.106.44:5000/download?url='),
+    'secondary': os.environ.get('SECONDARY_DOWNLOAD_API_URL', 'http://68.183.106.44:5001/download?url='),
 }
 
 # Caching setup for downloads
