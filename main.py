81f9eff4d302698e90a2b5f612677e8aeca959be
frozenoffical <tenopno@gmail.com>
2025-06-27 14:46:54 +0530

    Update main.py

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ app = Flask(__name__)
 
 # Define multiple Download APIs
 DOWNLOAD_APIS = {
-    'default': os.environ.get('DOWNLOAD_API_URL', 'https://frozen-youtube-api-search-link-b89x.onrender.com/download?url='),
+    'default': os.environ.get('DOWNLOAD_API_URL', 'http://68.183.106.44:5001/download?url='),
     'secondary': os.environ.get('SECONDARY_DOWNLOAD_API_URL', 'http://68.183.106.44:5000/download?url='),
 }
 
